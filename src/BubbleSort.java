24af5abfe5e70294a5ddffaeeb3c37bfe044e055
Arthur <1arthurluiz1@gmail.com>
2021-05-12 15:12:08 -0300

    Created BubbleSort method

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -3,6 +3,16 @@ package src;
 public class BubbleSort implements Sort{
     @Override
     public int[] sort(int[] nos) {
+        int aux;
+        for(int i = nos.length - 1; i > 0; i--){
+            for(int j = 0; j < i; j++){
+                if(nos[j] > nos[j + 1]){
+                    aux = nos[j];
+                    nos[j] = nos[j+1];
+                    nos[j+1] = aux;
+                }
+            }
+        }
         return nos;
     }
 }
