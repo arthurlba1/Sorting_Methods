57bd379fe1e224d87a46229f8ed043edc50881d9
Arthur <1arthurluiz1@gmail.com>
2021-05-12 15:39:53 -0300

    Created ShellSort method

--- src/ShellSort.java
+++ src/ShellSort.java
@@ -1,5 +1,19 @@
 package src;
 
-public class ShellSort {
-    
+public class ShellSort implements Sort{
+    @Override
+    public int[] sort(int[] nos) {
+
+        for(int gap = nos.length/2; gap > 0; gap /=2){
+            for(int i = gap; i < nos.length; i++){
+                int aux = nos[i];
+                int j;
+                for (j = i; j >= gap && nos[j - gap] > aux; j -= gap)
+                nos[j] = nos[j - gap];
+
+                nos[j] = aux;
+            }
+        }
+        return nos;
+    }
 }
