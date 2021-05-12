648322be67ce3e8ad9513bd148f004c00f83ffc3
Arthur <1arthurluiz1@gmail.com>
2021-05-12 15:22:38 -0300

    Created InsertionSort method

--- src/InsertionSort.java
+++ src/InsertionSort.java
@@ -1,5 +1,19 @@
 package src;
 
-public class InsertionSort {
-    
+public class InsertionSort implements Sort{
+    @Override
+    public int[] sort(int[] nos) {
+
+        for (int j = 1; j < nos.length; j++){
+            int key = nos[j];
+            int i = j-1;
+            while((i > -1) && (nos[i] > key)){
+                nos[i+1] = nos[i];
+                i--;
+            }
+            nos[i+1] = key;
+        }
+
+        return nos;
+    }
 }
