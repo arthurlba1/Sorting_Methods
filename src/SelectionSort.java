86bf3e5f5cf767970927f7a5407828f3f283d912
Arthur <1arthurluiz1@gmail.com>
2021-05-12 15:35:18 -0300

    Created SelectionSort method

--- src/SelectionSort.java
+++ src/SelectionSort.java
@@ -1,5 +1,20 @@
 package src;
 
-public class SelectionSort {
-    
+public class SelectionSort implements Sort {
+    @Override
+    public int[] sort(int[] nos) {
+
+        for (int i = 0; i < nos.length - 1; i++){
+            int index = i;
+            for(int j = i+1; j < nos.length; j++){
+                if (nos[j] < nos[index]){  
+                    index = j;
+                }
+            }
+            int smallerNumber = nos[index];
+            nos[index] = nos[i];
+            nos[i] = smallerNumber;
+        }  
+        return nos;
+    }
 }
