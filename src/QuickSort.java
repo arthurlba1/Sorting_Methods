26c53ff88652fb29d929d228337b6aa532218026
Arthur <1arthurluiz1@gmail.com>
2021-05-13 15:36:35 -0300

    Change method to iterative form

--- src/QuickSort.java
+++ src/QuickSort.java
@@ -1,47 +1,57 @@
 package src;
+import java.util.*;
 
 public class QuickSort implements Sort{
+ 
     @Override
     public int[] sort(int[] nos) {
-        int beg = 0;
-        int end = nos.length;
-
-        if(end > beg){
-            int indexPivot = slice(nos, beg, end);
-            end = indexPivot -1;
-            sort(nos);
-            beg = indexPivot +1;
-            sort(nos);
-        }
+        int low = 0;
+        int high = nos.length-1;
+        qSort(nos, low, high);
         return nos;
     }
+    private static int slice(int nos[], int low, int high){
+        int pivot = nos[high];
+  
+        int i = (low - 1);
+        for (int j = low; j <= high - 1; j++) {
+            if (nos[j] <= pivot) {
+                i++;
 
-    private int slice(int[] nos, int beg, int end){
-        int pivot, dLeft, dRight = end;
-        dLeft = beg + 1;
-        pivot = nos[beg];
-        
-        while(dLeft <= dRight) {
-            while(dLeft <= dRight && nos[dLeft] <= pivot) {
-                dLeft++;
+                int temp = nos[i];
+                nos[i] = nos[j];
+                nos[j] = temp;
             }
-            while(dRight >= dLeft && nos[dRight] > pivot) {
-                dRight--;
-            }
-            if(dLeft < dRight) {
-                change(nos, dRight, dLeft);
-                dLeft++;
-                dRight--;
-              }
-            }
-        
-            change(nos, beg, dRight);
-            return dRight;
         }
-        private void change(int[] nos, int i, int j) {
-            int temp = nos[i];
-            nos[i] = nos[j];
-            nos[j] = temp;
-          }
+  
+        int temp = nos[i + 1];
+        nos[i + 1] = nos[high];
+        nos[high] = temp;
+  
+        return i + 1;
     }
+
+    private static void qSort(int nos[], int low, int high){
+        int[] stack = new int[high - low + 1];
+        int top = -1;
+        stack[++top] = low;
+        stack[++top] = high;
+
+        while(top >= 0){
+            high = stack[top--];
+            low = stack[top--];
+
+            int p = slice(nos, low, high);
+
+            if(p-1 > low){
+                stack[++top] = low;
+                stack[++top] = p - 1;
+            }
+            if (p + 1 < high) {
+                stack[++top] = p + 1;
+                stack[++top] = high;
+            }
+        }
+    }
+}
 
